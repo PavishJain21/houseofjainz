749b9672eb01f3ba49b31d42a3c5aeac6e1ab048
pavishjain21 <jainpavish@gmail.com>
2025-02-13 13:40:44 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'houseofjain-z',
+  webDir: 'dist'
+};
+
+export default config;
